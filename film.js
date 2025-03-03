a21030c60f41a28af59a80fb7261cddc8b50e083
Alice Smith <alice@webage.com>
2025-03-03 13:09:55 -0500

    stuff

--- film.js
+++ film.js
@@ -9,17 +9,17 @@ let planetsUl;
 const baseUrl = `http://localhost:9001/api/films`;
 
 // Runs on page load
-addEventListener('DOMContentLoaded', () => {
-  producer = document.querySelector('span#Producer');
-  title = document.querySelector('h1#name');
-  episode_id = document.querySelector('span#Episode');
-  director = document.querySelector('span#Director');
-  release_date = document.querySelector('span#Released');
-  opening_crawl = document.querySelector('span#OpeningCrawl');
-  charactersUl = document.querySelector('ul#Characters_ID');
-  planetsUl = document.querySelector('ul#Planets_ID');
+addEventListener("DOMContentLoaded", () => {
+  producer = document.querySelector("span#Producer");
+  title = document.querySelector("h1#name");
+  episode_id = document.querySelector("span#Episode");
+  director = document.querySelector("span#Director");
+  release_date = document.querySelector("span#Released");
+  opening_crawl = document.querySelector("span#OpeningCrawl");
+  charactersUl = document.querySelector("span#Characters_ID");
+  planetsUl = document.querySelector("ul#Planets_ID");
   const sp = new URLSearchParams(window.location.search);
-  const id = sp.get('id');
+  const id = sp.get("id");
   getFilm(id);
 });
 
@@ -37,31 +37,28 @@ async function getFilm(id) {
 
 async function fetchCharacters(id) {
   let characterUrl = `${baseUrl}/${id}/characters`;
-  return await fetch(characterUrl)
-    .then(res => res.json());
+  return await fetch(characterUrl).then((res) => res.json());
 }
 
 async function fetchPlanets(id) {
   const url = `${baseUrl}/${id}/planets`;
-  const planets = await fetch(url)
-    .then(res => res.json());
+  const planets = await fetch(url).then((res) => res.json());
   return planets;
 }
 
 async function fetchFilms(id) {
   const url = `${baseUrl}/${id}`;
-  const film = await fetch(url)
-    .then(res => res.json());
+  const film = await fetch(url).then((res) => res.json());
   return film;
 }
 
-const renderFilm = film => {
+const renderFilm = (film) => {
   if (!film) {
-    console.error('No film data available');
+    console.error("No film data available");
     return;
   }
 
-  document.title = `SWAPI - ${film?.title}`;  // Just to make the browser tab say the film title
+  document.title = `SWAPI - ${film?.title}`; // Just to make the browser tab say the film title
   if (title) title.textContent = film?.title;
   if (producer) producer.textContent = film?.producer;
   if (episode_id) episode_id.textContent = film?.episode_id;
@@ -70,12 +67,17 @@ const renderFilm = film => {
   if (opening_crawl) opening_crawl.textContent = film?.opening_crawl;
 
   if (charactersUl) {
-    const charactersLis = film?.characters?.map(character => `<li><a href="/character.html?id=${character.id}">${character.name}</a></li>`);
+    const charactersLis = film?.characters?.map(
+      (character) =>
+        `<li><a href="/character.html?id=${character.id}">${character.name}</a></li>`
+    );
     charactersUl.innerHTML = charactersLis.join("");
   }
 
   if (planetsUl) {
-    const planetsLis = film?.planets?.map(planet => `<li>${planet.name}</li>`);
+    const planetsLis = film?.planets?.map(
+      (planet) => `<li>${planet.name}</li>`
+    );
     planetsUl.innerHTML = planetsLis.join("");
   }
-}
+};
